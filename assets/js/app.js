396d8c740280c1dd0ec1badf2668b28825e4ca1e
NadiaQN <quezada.nadia@gmail.com>
2018-02-06 18:19:38 -0300

    ejercicio terminado, version hacker incluida

---
+++ assets/js/app.js
@@ -0,0 +1,71 @@
+function initMap() {
+  var laboratoriaLima = {lat: -12.1191427, 
+    lng: -77.0349046};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: laboratoriaLima
+  });
+
+  var markadorLaboratoria = new google.maps.Marker({
+    position: laboratoriaLima,
+    map: map
+  });
+
+  function buscar() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+    }
+  }
+
+  var latitud, longitud;
+  var funcionExito = function(posicion) {
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
+    
+    var miUbicacion = new google.maps.Marker({
+      position: {
+        lat: latitud,
+        lng: longitud
+      },
+      map: map
+    });
+    map.setZoom(18);
+    map.setCenter({
+      lat: latitud,
+      lng: longitud
+    }); 
+  };
+
+  var funcionError = function(error) {
+    alert('Tenemos un problema con encontrar tu posición');
+  };
+  document.getElementById('encuentrame').addEventListener('click', buscar);
+  var inputPartida = document.getElementById('punto-partida');
+  var inputDestino = document.getElementById('punto-destino');
+
+  new google.maps.places.Autocomplete(inputPartida);
+  new google.maps.places.Autocomplete(inputDestino);
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
+  var calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: inputPartida.value,
+      destination: inputDestino.value,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('No encontramos una ruta');
+      }
+    });
+  };
+  directionsDisplay.setMap(map);
+  var trazarRuta = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('trazar-ruta').addEventListener('click', trazarRuta);
+};
+
